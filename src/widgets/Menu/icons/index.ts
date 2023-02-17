3d3d4129c530631c9266a0f265461d3abb231719
orangeswap <orangeswapfinance@protonmail.com>
2023-02-17 16:15:33 +0900

    Icons

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -25,3 +25,7 @@ export { default as RoadmapIcon } from "./Roadmap";
 export { default as BondsIcon } from "./Bonds";
 export { default as VaultsIcon } from "./Vaults";
 export { default as EggHouseIcon } from "./EggHouse";
+export { default as SwapIcon } from './SwapIcon';
+export { default as FarmsIcon } from './FarmsIcon';
+export { default as LabIcon } from './LabIcon';
+export { default as OraIcon } from './OraIcon';
